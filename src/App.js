5fc8497cbdc56be03c74ff6214405cebfd56314f
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-09-09 22:26:37 +0530

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { ThemeContext } from './Context/theme';
+import Aos from 'aos';
+import { Header } from './Components/Header/Header';
 
 function App() {
+  const [{ themename }] = React.useContext(ThemeContext);
+  React.useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="top" className={`${themename} app`}>
+      <section id="#home">
+        <Header />
+      </section>
+
     </div>
   );
 }
